fe1a3f4577d399a85bbe0e2391eb6e39196c8a6b
Kotha Sai Krishna <kotha.saikrishna07@gmail.com>
2025-01-14 16:40:49 +0530

    feat(2657): first attempt, using 2 sets

--- python3/medium/2657.find-the-prefix-common-array-of-two-arrays.py
+++ python3/medium/2657.find-the-prefix-common-array-of-two-arrays.py
@@ -9,18 +9,33 @@ from typing import *
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
         # approach 1
+        # n = len(A)
+        # ans = [0]*n
+        # freq = [0]*(n+1) # since in permutation numbers lies in range [1,n]
+        # common = 0
+        # for i in range(n):
+        #     freq[A[i]] += 1
+        #     if freq[A[i]] == 2:
+        #         common += 1
+        #     freq[B[i]] += 1
+        #     if freq[B[i]] == 2:
+        #         common += 1
+        #     ans[i] = common
+        # return ans
+
+        # approach 2
         n = len(A)
+        inA = set()
+        inB = set()
         ans = [0]*n
-        freq = [0]*(n+1) # since in permutation numbers lies in range [1,n]
-        common = 0
         for i in range(n):
-            freq[A[i]] += 1
-            if freq[A[i]] == 2:
-                common += 1
-            freq[B[i]] += 1
-            if freq[B[i]] == 2:
-                common += 1
-            ans[i] = common
+            inA.add(A[i])
+            inB.add(B[i])
+            c = 0
+            for e in inA:
+                if e in inB:
+                    c += 1
+            ans[i] = c
         return ans
 # @lc code=end
 
